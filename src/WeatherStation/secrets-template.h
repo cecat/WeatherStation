709ce0b1625faad02f64ca83423f4bb0b4aa3e13
cecat <cecatlett@gmail.com>
2023-12-30 08:03:29 -0600

    add template for secrets.h

---
+++ src/WeatherStation/secrets-template.h
@@ -0,0 +1,11 @@
+// secrets.h
+
+// wifi
+const char *WIFI_SSID   = "your-wifi-ssid";
+const char *WIFI_PWD    = "your-wifi-password";
+
+// HA MQTT
+const char *MQTT_USR    = "mqtt-username-if-needed";
+const char *MQTT_PWD    = "mqtt-password-if-needed";
+const char *MQTT_CLIENT = "mqtt-client-name";
+char MQTT_BROKER[]      = "mqtt-broker-server-name";
