7e2ceb1557be070156eed83809fb904f63b6ee28
cecat <cecatlett@gmail.com>
2023-12-31 12:27:10 -0600

    create template for secrets.h

---
+++ src/WeatherStationAdaFruit/secrets-template.h
@@ -0,0 +1,16 @@
+// secrets.h
+
+// wifi
+#define WLAN_SSID     "your-wifi-ssid"
+#define WLAN_PASS     "your-wifi-password""
+
+// HA MQTT
+// Adafruit IO Account Configuration. To obtain these values, visit
+// https://io.adafruit.com and click on the yellow key icon upper
+// right, just left of the "New Device" icon.  It appears that for
+// Adafruit rather than a client_ID you register devices to get a unique
+// key for the device.
+
+#define AIO_USERNAME  "your-{adafruit.io}-username""
+#define AIO_KEY       "your-{adafruit.io}-key""
+
