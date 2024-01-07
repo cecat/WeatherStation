54d509e1800eb2269159b77df28aeee0e93fcaff
cecat <cecatlett@gmail.com>
2024-01-07 13:31:49 -0600

    comments

--- src/WeatherStationAdaFruit/secrets-template.h
+++ src/WeatherStationAdaFruit/secrets-template.h
@@ -11,6 +11,11 @@
 // Adafruit rather than a client_ID you register devices to get a unique
 // key for the device.
 
-#define AIO_USERNAME  "your-{adafruit.io}-username""
-#define AIO_KEY       "your-{adafruit.io}-key""
+// If you create your own account you'll want to keep your credentials
+// here, deleting the duplicate lines from topics.h.  But for this
+// test code the values below are set in the topics.h file
+// so the lines below are commented out.
+
+//#define AIO_USERNAME  "your-{adafruit.io}-username""
+//#define AIO_KEY       "your-{adafruit.io}-key""
 
