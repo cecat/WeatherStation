49ced9a96fdcb96ce47ae4486b5f11f79822cd6f
cecat <cecatlett@gmail.com>
2024-01-08 09:13:09 -0600

    cleanup

--- src/WeatherStationAdaFruit/topics.h
+++ src/WeatherStationAdaFruit/topics.h
@@ -4,7 +4,24 @@
 
 /* this really has evolved in to way more than topics, to include MQTT settings
    and sensor variables, along with topics...
-   CeC (5-Jan-2024)
+   @cecat (5-Jan-2024)
+
+   Some reorganization below, your device-specific changes will go down
+   in the section beginning at "**** DEVICE CONFIG ****".  
+   
+   You'll also need to create your secrets.h file with local wifi name/password
+   and the shared username and password for io.adafruit.com.  Once
+   you confirm that you are reporting successfully to the waggletest group
+   at io.adafruit.com follow the instructions below to create your own
+   device ID. 
+   
+   Note: if you are using this code and not part of the waggle group
+   you will need to create a free account at io.adafruit.com and the rest
+   of the insturctions should translate reasonably well to what you need
+   to do.
+   
+   @cecat (8-Jan-2024)
+
   */
 
 // *********************** AdaFruit MQTT ******************** //
@@ -16,7 +33,8 @@
 // WiFiFlientSecure for SSL/TLS support
 WiFiClientSecure client;
 
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+// Setup the MQTT client class by passing in the WiFi client 
+// and MQTT server and login details. (username and key are in secrets.h)
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 // io.adafruit.com root CA
@@ -50,42 +68,30 @@ const char* adafruitio_root_ca = \
       "-----END CERTIFICATE-----\n";
 
 
-/****************************** Feeds ***************************************/
+/******************* Config struct for configuration ********************/
 
-// Setup a feed called 'test' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+/*  These fields allow (other MQTT implmentations) to construct the mqtt 
+    topic (to sub/pub) and to loop through the sensors using the proper
+    sampling code (via sensorIndex) and grabbing the sensor's latest reading
+    (sensorReading) for publishing.
 
-//Adafruit_MQTT_Publish test = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/test");
+    sensorIndex: may be overly general anticipating the possibility 
+    that multiple sensors might use the same method for reading the value
+    (e.g., simple analogRead sensors or multiple instances of, say
+    a soil moisture probe).
 
-// I tried to figure out a way to define these from our sensorArray below so we
-// would not have to manually align names/sensors/etc but could not (maybe someone else can?)
-// ******* NOTE: The names (xxxFeed) and feeds ("/feeds/xxx") MUST match the 
-// *******       sensorArray list below and must also be manually inserted into the switch()
-// *******       statement in the function publishSensorData().
-Adafruit_MQTT_Publish rainFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.rain");
-Adafruit_MQTT_Publish wind_dirFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.winddir");
-Adafruit_MQTT_Publish wind_speedFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.windspeed");
-Adafruit_MQTT_Publish soilFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.soil");
+    sensorName and sensorVar: other than the Serial.print in 
+    publishSensorData(), these are not used here with AdaFruit, but in
+    other MQTT implementations we use them to dynamically
+    create topics. One could remove them here (along with the
+    Serial.print invocations in publishSensorData()) without any
+    issues. But for now they at least help humans reading the code
+    to follow which sensor is which
 
-
-// *********************** Sensors ************************** //
-
-//Hardware pin definitions
-const byte WSPEED = 14;     //Digital I/O pin for wind speed
-const byte WDIR = 35;       //Analog pin for wind direction
-const byte RAIN = 27;       //Digital I/O pin for rain fall
-
-/*
-    These fields allow us to construct the mqtt topic (to sub/pub)
-    and also to loop through the sensors using the proper code for
-    sampling (via sensorIndex) and grabbing the sensor's latest reading
-    (sensorReading).
-
-    sensorIndex is probably thinking too generally in that there might be
-    more than one sensor that uses the same method for reading the value
-    (perhaps if you had some simple analogRead sensors). I am still
-    tempted to drop this field and just use the index in the switch
-    statement...
+    sensorReading: given that AdaFruit wants a String as the payload (sensor reading)
+    it might make sense to have sensorReading be a String here to avoid
+    having to convert ints. This would just shift the conversion-to-String
+    but does not avoid it, so it's probably 6 one way 3! the other.
  */
 struct SensorConfig {
     int sensorIndex;      // arbitrary indexing to determine how to read;
@@ -94,29 +100,60 @@ struct SensorConfig {
     String sensorVar;     // sensor measurement name
     float sensorReading;  // latest reading from this sensor
   };
-  
+
+/************************* DEVICE CONFIG *********************/
+
+/************************* Hardware **************************/
+//Hardware pin assignments
+const byte WSPEED = 14;     //Digital I/O pin for wind speed
+const byte WDIR   = 35;     //Analog pin for wind direction
+const byte RAIN   = 27;     //Digital I/O pin for rain fall
+
+/************************** Feeds *****************************/
+/*  I have tried multiple strategies to define these from our 
+    sensorArray below and/or store them in the array, but to
+    no avail. Absent that stratgegy we must manually align
+    names/sensors/etc (maybe someone else can try?)
+    THUS: The names (xxxFeed) and feeds ("/feeds/xxx") MUST align
+          with the sensorArray list below and with the switch()
+          statement in the function publishSensorData().
+    AND: note that "waggletest" is the group name at io.adafruit.com
+         for testing purposes. When you have things working, create a
+         three-letter device name (e.g., your initials) and a
+         corresponding GROUP at io.adafruit.com for your feeds, then
+         replace "waggletest" with your group name.
+ */
+Adafruit_MQTT_Publish rainFeed = 
+    Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.rain");
+Adafruit_MQTT_Publish wind_dirFeed = 
+    Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.winddir");
+Adafruit_MQTT_Publish wind_speedFeed = 
+    Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.windspeed");
+Adafruit_MQTT_Publish soilFeed =  
+    Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/waggletest.soil");
+
+/********************* SENSOR CONFIGURATION ********************************/
+
 // Configuration for this device
-
-// Create your own 3-character ID such as your 3-letter initials
-
-#define DEVICE_ID "CEC"   
-  
 SensorConfig sensorArray[] = {
     {0, "CN",     "rain",       0.0 },          
     {1, "CN",     "wind_dir",   0.0 },  
     {2, "CN",     "wind_speed", 0.0 },   
     {3, "AS3935", "soil",       0.0 }
 };
-/* Suggested for other sensors that we expect - move into the
-   SensorConfig array list above, making sure to align the sensorIndex
-   with the code/functions for sampling/reading and reporting
 
-    {4, "AS3935",        "Lightning"},
-    {5, "VEML075",       "UVA"},
-    {6, "VEML075",       "UVB"},
-    {7, "BME280",        "RelativeHumidity"},
-    {8, "BME280",        "Temperature"},
-    {9, "bME280",        "BarometricPressure"},
+/* Other sensors that we expect - move into the
+   SensorConfig array list above, making sure to align the sensorIndex
+   with the code/functions for sampling/reading and reporting.
+   (also make sure to include their pin assigments above in 
+   "*** Hardware ***")
+
+    {4, "AS3935",  "Lightning",           0.0 },
+    {5, "VEML075", "UVA",                 0.0 },
+    {6, "VEML075", "UVB",                 0.0 },
+    {7, "BME280",  "RelativeHumidity",    0.0 },
+    {8, "BME280",  "Temperature",         0.0 },
+    {9, "bME280",  "BarometricPressure",  0.0 },
 
  */
 
